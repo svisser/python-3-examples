fd3102f9275f65a669fb50580e9a00a9a2844409
Simeon Visser <simeon@ometria.com>
2014-09-07 01:51:32 +0100

    Move basic generators inside the example generator

--- examples/generators.py
+++ examples/generators.py
@@ -1,14 +1,4 @@
 
-def get_uppercase():
-    for c in range(26):
-        yield chr(65 + c)
-
-
-def get_lowercase():
-    for c in range(26):
-        yield chr(97 + c)
-
-
 def get_uppercase_and_lowercase():
     """
     As of Python 3.3 you can use 'yield from' to yield values from
@@ -18,5 +8,13 @@ def get_uppercase_and_lowercase():
     for c in get_uppercase_and_lowercase():
         print(c)
     """
+    def get_uppercase():
+        for c in range(26):
+            yield chr(65 + c)
+
+    def get_lowercase():
+        for c in range(26):
+            yield chr(97 + c)
+
     yield from get_uppercase()
     yield from get_lowercase()
